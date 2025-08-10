a968d27b9c6b87000f5cdd282f068891f1e6ee4c
Edgardo Torres <ejtf99usa@gmail.com>
2025-08-10 10:03:15 -0400

    adding media queries and fixing small things

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Budget {
   constructor() {
+    //Arrays to store individual incomes and expenses
     this.incomes = [];
     this.expenses = [];
     this.totalIncome = 0;
@@ -8,13 +9,18 @@ class Budget {
 
   addIncome(description, amount) {
     if (description && amount > 0) {
+      //Store income as an object in the array
       this.incomes.push({ description, amount });
+      // increase the income
       this.totalIncome += amount;
+      //Update the income in the screen
       this.updateBuget();
+      // show it as list in the tracker
       this.renderItem('income', description, amount)
     }
   }
 
+  //Same logic as addIncome
   addExpense(description, amount) {
     if (description && amount > 0) {
       this.expenses.push({ description, amount });
@@ -23,14 +29,18 @@ class Budget {
       this.renderItem('expense', description, amount)
     }
   }
+  //Refresh displayed totals
   updateBuget() {
+    //Calculate the avaliable money in the budget
     const totalBuget = this.totalIncome - this.totalExpenses;
+    // Get the element to manipulate the DOM
     document.getElementById("available-money").innerText = totalBuget;
     document.getElementById("earned").innerText = this.totalIncome;
     document.getElementById("spent").innerText = this.totalExpenses;
   }
-
+// create and put the new list item in the DOM
   renderItem(type, description, amount) {
+    //pick the correct list
     const container =
       type === "income"
         ? document.querySelector(".income-list")
@@ -41,28 +51,32 @@ class Budget {
             return;
         }
 
+        // create the outer div
     const createDiv = document.createElement("div");
     createDiv.classList.add("item");
 
+    //add description element
     const descriptionElement = document.createElement("h4");
     descriptionElement.innerText = description;
 
+    //add amount element
     const amountElement = document.createElement("div");
     amountElement.classList.add(`item-${type}`);
     amountElement.innerHTML = `<p class="${type}-symbol">$</p><span class="${type}-amount">${amount}</span>`;
-
+        // add delete icon
     const deleteButton = document.createElement("i");
     deleteButton.className ="bx bx-trash";
     deleteButton.addEventListener("click", () => {
       container.removeChild(createDiv);
       this.removeItem(type, amount);
     });
-
+    // create the iteam
     createDiv.appendChild(descriptionElement);
     createDiv.appendChild(amountElement);
     createDiv.appendChild(deleteButton);
     container.appendChild(createDiv);
   }
+  // remove an item from array and adjust total
   removeItem(type, amount){
     if(type === 'income'){
         this.totalIncome -= amount;
@@ -74,21 +88,23 @@ class Budget {
     this.updateBuget();
 }}
 
+// When page finishis loading
 document.addEventListener("DOMContentLoaded", () => {
   const budget = new Budget();
-
+// income button click
   document.getElementById("add-income").addEventListener("click", () => {
     const description = document.getElementById("description").value;
     const amount = parseFloat(document.getElementById("amount").value);
     if (description && amount > 0) {
       budget.addIncome(description, amount);
+      //clear inputs
       document.getElementById("description").value = "";
       document.getElementById("amount").value = "";
     } else {
       alert("Please enter a valid income description and amount.");
     }
   });
-
+// Expenses button click
   document.getElementById("add-expense").addEventListener("click", () => {
     const description = document.getElementById("description").value;
     const amount = parseFloat(document.getElementById("amount").value);
